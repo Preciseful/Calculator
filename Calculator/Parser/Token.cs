d77b7dc279f9647f65ae25382e878063abe53a7a
Developful <preciseful1@gmail.com>
2023-09-15 21:29:12 +0300

    other

---
+++ Calculator/Parser/Token.cs
@@ -0,0 +1,11 @@
+﻿namespace Developful.Parser; 
+
+public class Token {
+    public string Symbol;
+    public string Type;
+
+    public Token(string symbol, string type) {
+        this.Symbol = symbol;
+        this.Type = type;
+    }
+}
