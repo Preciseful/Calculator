4f9c2012d0a96fbf716ed81fca2aba42590faa5e
Developful <preciseful1@gmail.com>
2023-09-15 22:53:49 +0300

    Moved Token.cs from Calculator\Parser to Calculator\src

---
+++ Calculator/src/Token.cs
@@ -0,0 +1,16 @@
+﻿namespace Developful.Parser; 
+
+public class Token {
+    public string Symbol;
+    public string Type;
+
+    public Token(string symbol, string type) {
+        this.Symbol = symbol;
+        this.Type = type;
+    }
+
+    public static bool operator ==(Token x, Token y) => x.Type == y.Type;
+    public static bool operator !=(Token x, Token y) => x.Type != y.Type;
+    public static bool operator ==(Token x, string y) => x.Type == y;
+    public static bool operator !=(Token x, string y) => x.Type != y;
+}
